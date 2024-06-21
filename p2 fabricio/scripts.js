70602897d7f4f49ae71cfb901f387a942ca599b0
ElissonJBrito <elissonjbrito@gmail.com>
2024-06-21 20:51:02 -0300

    Add files via upload

---
+++ p2 fabricio/scripts.js
@@ -0,0 +1,60 @@
+let slideIndex = 1;
+
+function openLightbox() {
+    document.getElementById('lightbox').style.display = 'flex';
+    showSlides(slideIndex);
+}
+
+function closeLightbox() {
+    document.getElementById('lightbox').style.display = 'none';
+}
+
+function changeSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName('lightbox-slide');
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    slides[slideIndex - 1].style.display = 'block';
+}
+
+// Sidebar
+function toggleSidebar() {
+    let sidebar = document.getElementById('sidebar');
+    if (sidebar.style.display === 'block') {
+        sidebar.style.display = 'none';
+    } else {
+        sidebar.style.display = 'block';
+    }
+}
+
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+  }
+  
+  function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+  }
+  
+// Accordion
+let acc = document.getElementsByClassName('accordion-button');
+for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener('click', function() {
+        this.classList.toggle('active');
+        let content = this.nextElementSibling;
+        if (content.style.display === 'block') {
+            content.style.display = 'none';
+        } else {
+            content.style.display = 'block';
+        }
+    });
+}
